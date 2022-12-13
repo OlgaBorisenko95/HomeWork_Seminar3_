a6ab55d66d644b63d31a7005f0347a42895222fe
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-13 18:45:45 +0300

    Нашли расстояние между точками

---
+++ task2/расстояние между точками.cs
@@ -0,0 +1,25 @@
+﻿
+
+int x1 = EnterData("Введите координату x1: ");
+int y1 = EnterData("Введите координату y1: ");
+int z1 = EnterData("Введите координату z1: ");
+int x2 = EnterData("Введите координату x2: ");
+int y2 = EnterData("Введите координату y2: ");
+int z2 = EnterData("Введите координату z2: ");
+
+double FindDistance(int x1, int y1, int x2, int y2, int z1, int z2)
+{  
+    double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2 + Math.Pow(z2-z1, 2)));
+    return result;
+}
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+double result = FindDistance(x1, y1, x2, y2, z1, z2);
+
+
+Console.WriteLine($"Расстояние между заданными точками равно {result}");
